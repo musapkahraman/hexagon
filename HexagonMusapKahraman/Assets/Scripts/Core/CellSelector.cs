d49cd013d48e7734b9baef3fb1a6d63186f99a39
Musap Kahraman <musapkahraman@gmail.com>
2020-09-11 23:35:15 +0300

    Initial commit

---
+++ HexagonMusapKahraman/Assets/Scripts/Core/CellSelector.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace HexagonMusapKahraman.Core
+{
+    public class CellSelector : MonoBehaviour, IPointerDownHandler, IDragHandler
+    {
+        [SerializeField] private TMP_Text textfield;
+        private Camera _camera;
+        private Grid _grid;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+            _grid = GetComponentInParent<Grid>();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            textfield.text = $"Pointer {GetClickedCell(eventData.position).ToString()}";
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            textfield.text = $"Dragging {GetClickedCell(eventData.position).ToString()}";
+        }
+
+        private Vector3Int GetClickedCell(Vector2 position)
+        {
+            var worldPoint = _camera.ScreenToWorldPoint(position);
+            return _grid.WorldToCell(worldPoint);
+        }
+    }
+}
