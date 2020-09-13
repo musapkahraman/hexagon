a1fcb751dfd9f7e40cc6a7639a4064474bf7a027
Musap Kahraman <musapkahraman@gmail.com>
2020-09-13 19:37:11 +0300

    feat: Add replay button's functionality

---
+++ HexagonMusapKahraman/Assets/Scripts/UI/ReplayButtonController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HexagonMusapKahraman.UI
+{
+    public class ReplayButtonController : MonoBehaviour
+    {
+        public void Replay()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
